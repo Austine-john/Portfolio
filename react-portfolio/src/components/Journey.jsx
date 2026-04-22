c278459426b4a3d4320875b46b4d615ab933741b
Austine-john <ochiengaustine582@gmail.com>
2026-04-22 16:04:02 +0300

    changed caroussel journey to stepper

--- react-portfolio/src/components/Journey.jsx
+++ react-portfolio/src/components/Journey.jsx
@@ -20,16 +20,16 @@ const Journey = () => {
                     <div className="journey-item">
                         <div className="journey-icon"><FaBriefcase /></div>
                         <div className="journey-content">
-                            <h4>Full-Stack Developer</h4>
-                            <p className="journey-company">Freelance | 2022 - Present</p>
+                            <h4>Junior Quality Assurance Engineer</h4>
+                            <p className="journey-company">Konvergenz Network Solutions | Oct 2025 - Present</p>
                         </div>
                     </div>
 
                     <div className="journey-item">
-                        <div className="journey-icon"><FaGraduationCap /></div>
+                        <div className="journey-icon"><FaBriefcase /></div>
                         <div className="journey-content">
-                            <h4>Bachelor of Science in Computer Science</h4>
-                            <p className="journey-company">Cooperative University Of Kenya | Sep 2021 - Dec 2025</p>
+                            <h4>Full-Stack Developer</h4>
+                            <p className="journey-company">Freelance | 2022 - Present</p>
                         </div>
                     </div>
 
@@ -42,10 +42,10 @@ const Journey = () => {
                     </div>
 
                     <div className="journey-item">
-                        <div className="journey-icon"><FaBriefcase /></div>
+                        <div className="journey-icon"><FaGraduationCap /></div>
                         <div className="journey-content">
-                            <h4>Junior Quality Assurance Engineer</h4>
-                            <p className="journey-company">Konvergenz Network Solutions | Oct 2025 - Present</p>
+                            <h4>Bachelor of Science in Computer Science</h4>
+                            <p className="journey-company">Cooperative University Of Kenya | Sep 2021 - Dec 2025</p>
                         </div>
                     </div>
 
@@ -56,8 +56,7 @@ const Journey = () => {
                             <p className="journey-company">Smart Applications International | May 2024 - Aug 2024</p>
                         </div>
                     </div>
-                    {/* Spacer to ensure last card is fully visible */}
-                    <div style={{ minWidth: '2rem', flexShrink: 0 }}></div>
+
                 </div>
             </div>
         </section>
